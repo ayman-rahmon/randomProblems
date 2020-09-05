b63ab76e7f945d7675a3c142996f8821907b3bbc
Ayman Rahmon <ayman_rahmon@hotmail.com>
2020-09-05 22:47:55 +0300

    kangaroo stupid solution...

--- src/Kangroo.java
+++ src/Kangroo.java
@@ -7,7 +7,20 @@ public class Kangroo {
 
 
 
+    static String kangaroo(int x1, int v1, int x2, int v2) {
 
+        int counter = 0 ;
+            // (x1<=10000) || (x2<=10000)
+        while(counter < 10000) {
+            if((x1 ) == (x2) ) {
+                return "YES" ;
+            }
+            x1 += v1 ;
+            x2 += v2 ;
+            counter++ ;
+        }
+        return "NO" ;
+    }
 
 
 
